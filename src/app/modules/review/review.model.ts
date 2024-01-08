01b3f420435827c678c9b94bb508d3b83d28392d
Shakil53 <shakilahmed.dcc@gmail.com>
2024-01-08 22:15:41 +0600

    review module interface, model, validation

---
+++ src/app/modules/review/review.model.ts
@@ -0,0 +1,18 @@
+import { model, Schema } from 'mongoose';
+import { TReview } from './review.interface';
+
+const reviewSchema = new Schema<TReview>({
+  courseId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Course',
+    required: [true, 'Course id is required'],
+  },
+  rating: { type: Number, required: [true, 'Rating is required'] },
+  review: { type: String },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+
+export const Review = model<TReview>('Review', reviewSchema);
