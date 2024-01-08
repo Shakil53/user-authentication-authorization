f410828ea4e65ab23c63fe1ebc82a857e53828a1
Shakil53 <shakilahmed.dcc@gmail.com>
2024-01-08 22:02:02 +0600

    category controller, route,validation

---
+++ src/app/modules/category/category.service.ts
@@ -0,0 +1,30 @@
+import { JwtPayload } from 'jsonwebtoken';
+import { TCategory } from './category.interface';
+import { Category } from './category.model';
+
+// create category
+const createCategory = async (
+  payload: TCategory,
+  categoryCreator: JwtPayload,
+) => {
+  const newCategory = {
+    name: payload.name,
+    createdBy: categoryCreator._id,
+  };
+
+  const result = await Category.create(newCategory);
+
+  return result;
+};
+
+// get all categories
+const getAllCategories = async () => {
+  const result = await Category.find().populate('createdBy');
+
+  return result;
+};
+
+export const categoryServices = {
+  createCategory,
+  getAllCategories,
+};
